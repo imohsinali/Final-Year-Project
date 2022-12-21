d87e07ae31e21ef24cc706c405e126e3939aa437
Mohsin Ali <59974279+imohsinali@users.noreply.github.com>
2022-12-21 23:11:59 +0500

    Create landcontract.sol

---
+++ contract/landcontract.sol
@@ -0,0 +1,96 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity >=0.8.7 <=0.9.7;
+
+
+contract Land {
+    address contractOwner;
+
+    constructor() public{
+        contractOwner = msg.sender;
+    }
+
+    struct Landreg {
+        uint id;
+        uint area;
+        string landAddress;
+        uint landPrice;
+        string allLatitudeLongitude;
+        //string allLongitude;
+        uint propertyPID;
+        string physicalSurveyNumber;
+        string document;
+        bool isforSell;
+        address payable ownerAddress;
+        bool isLandVerified;
+    }
+
+    struct User{
+        address id;
+        string name;
+        uint age;
+        string city;
+        string aadharNumber;
+        string panNumber;
+        string document;
+        string email;
+        bool isUserVerified;
+    }
+
+    struct LandInspector {
+        uint id;
+        address _addr;
+        string name;
+        uint age;
+        string designation;
+        string city;
+    }
+
+    struct LandRequest{
+        uint reqId;
+        address payable sellerId;
+        address payable buyerId;
+        uint landId;
+        reqStatus requestStatus;
+        bool isPaymentDone;
+    }
+    enum reqStatus {requested,accepted,rejected,paymentdone,commpleted}
+
+
+    uint inspectorsCount;
+    uint public userCount;
+    uint public landsCount;
+    uint public documentId;
+    uint requestCount;
+
+
+    mapping(address => LandInspector) public InspectorMapping;
+    mapping(uint => address[]) allLandInspectorList;
+    mapping(address => bool)  RegisteredInspectorMapping;
+    mapping(address => User) public UserMapping;
+    mapping(uint => address)  AllUsers;
+    mapping(uint => address[])  allUsersList;
+    mapping(address => bool)  RegisteredUserMapping;
+    mapping(address => uint[])  MyLands;
+    mapping(uint => Landreg) public lands;
+    mapping(uint => LandRequest) public LandRequestMapping;
+    mapping(address => uint[])  MyReceivedLandRequest;
+    mapping(address => uint[])  MySentLandRequest;
+    mapping(uint => uint[])  allLandList;
+    mapping(uint => uint[])  paymentDoneList;
+
+
+    function isContractOwner(address _addr) public view returns(bool){
+        if(_addr==contractOwner)
+            return true;
+        else
+            return false;
+    }
+
+    function changeContractOwner(address _addr)public {
+        require(msg.sender==contractOwner,"you are not contractOwner");
+
+        contractOwner=_addr;
+    }
+
+}
