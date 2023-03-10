e721feea6b0accc655aab9267c8615f582304ab0
Mohsin Ali <59974279+imohsinali@users.noreply.github.com>
2023-03-10 23:59:35 +0500

    Update Context.js

--- src/StateMangement/Context.js
+++ src/StateMangement/Context.js
@@ -6,11 +6,15 @@ export const TransactionContext = React.createContext();
 
 const { ethereum } = window;
 // const contractAddress="0x0C84632F86C08850Db4BbFC2b2C0f4AddB19EB79"
- const contractAddress = "0x71c1a7AE879D0E47d0987E6afd409b79B9Ae8f3D";
+const contractAddress = "0x71c1a7AE879D0E47d0987E6afd409b79B9Ae8f3D";
 const createEthereumContract = () => {
   const provider = new ethers.providers.Web3Provider(ethereum);
   const signer = provider.getSigner();
-  const transactionsContract = new ethers.Contract(contractAddress, contractABI.abi, signer);
+  const transactionsContract = new ethers.Contract(
+    contractAddress,
+    contractABI.abi,
+    signer
+  );
 
   return transactionsContract;
 };
@@ -21,13 +25,9 @@ export const TransactionsProvider = ({ children }) => {
   // const [isLoading, setIsLoading] = useState(false);
   // const [transactionCount, setTransactionCount] = useState(localStorage.getItem("transactionCount"));
   // const [transactions, setTransactions] = useState(localStorage.getItem('transaction'));
-  const [contract,setContract]=useState()
-  const [provider,setProvider]=useState()
+  const [contract, setContract] = useState();
+  const [provider, setProvider] = useState();
 
-
-  
-
- 
   const checkIfWalletIsConnect = async () => {
     try {
       if (!ethereum) return alert("Please install MetaMask.");
@@ -35,15 +35,18 @@ export const TransactionsProvider = ({ children }) => {
       const accounts = await ethereum.request({ method: "eth_accounts" });
       const provider = new ethers.providers.Web3Provider(ethereum);
       const signer = provider.getSigner();
-       const transactionsContract = new ethers.Contract(contractAddress, contractABI.abi, signer);
-       setContract(transactionsContract)
-       setProvider(provider)
-       console.log("hello 2", provider)
-       console.log("hello", transactionsContract)
+      const transactionsContract = new ethers.Contract(
+        contractAddress,
+        contractABI.abi,
+        signer
+      );
+      setContract(transactionsContract);
+      setProvider(provider);
+      console.log("hello 2", provider);
+      console.log("hello", transactionsContract);
 
       if (accounts.length) {
         setCurrentAccount(accounts[0]);
-
       } else {
         console.log("No accounts found");
       }
@@ -56,8 +59,9 @@ export const TransactionsProvider = ({ children }) => {
       if (ethereum) {
         const transactionsContract = createEthereumContract();
 
-        const availableTransactions = await transactionsContract.ReturnAllLandIncpectorList()
-         console.log(availableTransactions)
+        const availableTransactions =
+          await transactionsContract.ReturnAllLandIncpectorList();
+        console.log(availableTransactions);
         // const structuredTransactions = availableTransactions.map((transaction) => ({
         //   name: transaction.name,
         //   age: parseInt(transaction.age._hex),
@@ -70,56 +74,79 @@ export const TransactionsProvider = ({ children }) => {
         // console.log(structuredTransactions);
 
         // setTransactions(structuredTransactions);
-
       } else {
         console.log("Ethereum is not present");
       }
-
-      
     } catch (error) {
       console.log(error);
     }
   };
 
+   // console.log(checkIfWalletIsConnect())
+   const connectWallet = async () => {
+     try {
+       setProcessing(true);
 
+       // Check if the `ethereum` object is available
+       if (!window.ethereum) {
+         throw new Error("Please install MetaMask");
+       }
 
-  const connectWallet = async () => {
-    try {
-      if (!ethereum) return alert("Please install MetaMask.");
+       // Prompt the user to connect their wallet
+       await window.ethereum.enable();
 
-      const accounts = await ethereum.request({ method: "eth_requestAccounts", });
+       // Check if the user has already granted permission
+       const accounts = await window.ethereum.request({
+         method: "eth_accounts",
+       });
+       if (accounts.length === 0) {
+         throw new Error("Please connect your MetaMask wallet");
+       }
 
-      setCurrentAccount(accounts[0]);
-      const provider = new ethers.providers.Web3Provider(ethereum);
-      const signer = provider.getSigner();
-       const transactionsContract = new ethers.Contract(contractAddress, contractABI.abi, signer);
-       setContract(transactionsContract)
+       // Get the current account and set up the contract
+       const provider = new ethers.providers.Web3Provider(window.ethereum);
+       const signer = provider.getSigner();
+       const transactionsContract = new ethers.Contract(
+         contractAddress,
+         contractABI.abi,
+         signer
+       );
+       setCurrentAccount(accounts[0]);
+       setContract(transactionsContract);
 
+       // Reload the page (optional)
+       // window.location.reload();
+     } catch (error) {
+       console.log(error);
+       throw error;
+     }
+   };
 
-      // window.location.reload();
-    } catch (error) {
-      console.log(error);
-
-      throw new Error("No ethereum object");
-    }
-  };
-  // console.log(checkIfWalletIsConnect())
 
 
   useEffect(() => {
     checkIfWalletIsConnect();
-    getAllTransactions()
+    getAllTransactions();
   }, []);
+  const [coordinates, setCoordinates] = useState("");
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const [processing, setProcessing] = useState(false);
+
 
   return (
     <TransactionContext.Provider
       value={{
         connectWallet,
         checkIfWalletIsConnect,
+        setCoordinates,
         currentAccount,
-        // transactions,
         contract,
-        provider
+        provider,
+        coordinates,
+        isModalVisible,
+        setIsModalVisible,
+        processing,
+        setProcessing,
       }}
     >
       {children}
